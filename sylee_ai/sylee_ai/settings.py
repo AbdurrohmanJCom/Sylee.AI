1310268aab850ff0b382faf8cdf2ec43006b4b60
AbdurrohmanJCom <tuckerticket@gmail.com>
2025-03-04 03:55:38 +0800

    jwt lifetime 20years

--- sylee_ai/sylee_ai/settings.py
+++ sylee_ai/sylee_ai/settings.py
@@ -133,8 +133,8 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(hours=2), 
-    'REFRESH_TOKEN_LIFETIME': timedelta(days=6),
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=7300),  # 2 года
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=7300),  # 2 года
     'ROTATE_REFRESH_TOKENS': False,
     'BLACKLIST_AFTER_ROTATION': True,
     'ALGORITHM': 'HS256',
@@ -149,6 +149,6 @@ SIMPLE_JWT = {
     'TOKEN_TYPE_CLAIM': 'token_type',
     'JTI_CLAIM': 'jti',
     'SLIDING_TOKEN_REFRESH_EXP_CLAIM': 'refresh_exp',
-    'SLIDING_TOKEN_LIFETIME': timedelta(minutes=5),
-    'SLIDING_TOKEN_REFRESH_LIFETIME': timedelta(days=1),
-}   
+    'SLIDING_TOKEN_LIFETIME': timedelta(days=7300),  # 2 года
+    'SLIDING_TOKEN_REFRESH_LIFETIME': timedelta(days=7300),  # 2 года
+}       
